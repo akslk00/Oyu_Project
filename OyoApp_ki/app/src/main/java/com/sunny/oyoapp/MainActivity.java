df912459a2e081d8af50ce6a7ec9af8b5587237e
isk03300 <isk03300@naver.com>
2024-01-29 16:29:52 +0900

    add

--- OyoApp_ki/app/src/main/java/com/sunny/oyoapp/MainActivity.java
+++ OyoApp_ki/app/src/main/java/com/sunny/oyoapp/MainActivity.java
@@ -6,24 +6,17 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.kakao.sdk.user.UserApiClient;
 import com.sunny.oyoapp.config.Config;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,7 +36,8 @@ public class MainActivity extends AppCompatActivity {
         btnScrap = findViewById(R.id.btnScrap);
         btnMenu = findViewById(R.id.btnMenu);
 
-//        Log.d("getKeyHash", "" + getKeyHash(MainActivity.this));
+
+
 
 
         btnChat.setOnClickListener(new View.OnClickListener() {
@@ -77,48 +71,25 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+
+
         getSupportActionBar().hide();
 
 
         // TODO:토큰 생기면 그때 진행.
 
-//        SharedPreferences sp = getSharedPreferences(Config.PREFERENCE_NAME,MODE_PRIVATE);
-//        String token = sp.getString("token","");
-//
-//        if(token.isEmpty()){
-//            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-//            startActivity(intent);
-//            finish();
-//            return;
-//        }
+        SharedPreferences sp = getSharedPreferences(Config.PREFERENCE_NAME,MODE_PRIVATE);
+        String token = sp.getString("token","");
+
+        if(token.isEmpty()){
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
 
 
     }
-
-//    카카오 로그인 API 키 해시값 알아내는 코드
-
-//    public static String getKeyHash(final Context context) {
-//        PackageManager pm = context.getPackageManager();
-//        try {
-//            PackageInfo packageInfo = pm.getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES);
-//            if (packageInfo == null)
-//                return null;
-//
-//            for (Signature signature : packageInfo.signatures) {
-//                try {
-//                    MessageDigest md = MessageDigest.getInstance("SHA");
-//                    md.update(signature.toByteArray());
-//                    return android.util.Base64.encodeToString(md.digest(), android.util.Base64.NO_WRAP);
-//                } catch (NoSuchAlgorithmException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        } catch (PackageManager.NameNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//
-//    }
 
 
 }
--- OyoApp_ki/app/src/main/java/com/sunny/oyoapp/config/Config.java
+++ OyoApp_ki/app/src/main/java/com/sunny/oyoapp/config/Config.java
@@ -3,21 +3,18 @@ import android.app.Application;
 
 import com.kakao.sdk.common.KakaoSdk;
 
-public class Config {
+public class Config extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        KakaoSdk.init(this,"124b8a7439efa7895c1e49efb15d15ac");
+    }
 
     public static final String PREFERENCE_NAME = "oyo_app";
     public static final String DOMAIN = "https://pmqzo7zqwh.execute-api.ap-northeast-2.amazonaws.com";
     public static final String DOMAIN_LOCAL = "http://localhost:5000";
 
-    public class kakaoApplication extends Application {
-
-        @Override
-        public void onCreate() {
-            super.onCreate();
-
-            KakaoSdk.init(this,"124b8a7439efa7895c1e49efb15d15ac");
-        }
-    }
 
 
 }
