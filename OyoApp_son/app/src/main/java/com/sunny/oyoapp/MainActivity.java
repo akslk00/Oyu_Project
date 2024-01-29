67ec9dbf68bad25aedd4043a9149ab0cf84c05ba
YoonGeonSON <151480776+YoonGeonSON@users.noreply.github.com>
2024-01-29 15:24:20 +0900

    555

---
+++ OyoApp_son/app/src/main/java/com/sunny/oyoapp/MainActivity.java
@@ -0,0 +1,92 @@
+package com.sunny.oyoapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.sunny.oyoapp.config.Config;
+
+public class MainActivity extends AppCompatActivity {
+
+    LinearLayout btnHome;
+    LinearLayout btnChat;
+    LinearLayout btnScrap;
+    LinearLayout btnMenu;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnHome = findViewById(R.id.btnHome);
+        btnChat = findViewById(R.id.btnChat);
+        btnScrap = findViewById(R.id.btnScrap);
+        btnMenu = findViewById(R.id.btnMenu);
+
+
+
+        btnChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, ChatbotActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btnScrap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, ScarpActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+        getSupportActionBar().hide();
+
+
+        // TODO:토큰 생기면 그때 진행.
+
+//        SharedPreferences sp = getSharedPreferences(Config.PREFERENCE_NAME,MODE_PRIVATE);
+//        String token = sp.getString("token","");
+//
+//        if(token.isEmpty()){
+//            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//            startActivity(intent);
+//            finish();
+//            return;
+//        }
+
+
+    }
+
+
+}
